c7f27141063d7c3ff5b63736a08924371c222f10
ayoade <auadesope@gmail.com>
2024-07-16 17:01:20 +0100

    second commit

--- lib/services/storage _service.dart
+++ lib/services/storage _service.dart
@@ -1,38 +1,3 @@
-//
-// import 'dart:convert';
-// import 'dart:ui';
-// import 'package:shared_preferences/shared_preferences.dart';
-// import '../provider/drawing_provider.dart';
-//
-// class StorageService {
-//   Future<void> saveDrawing(String filename, List<Offset> points, List<TextData> texts, Color color, Color backgroundColor) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     final pointsData = points.map((e) => {'dx': e.dx, 'dy': e.dy}).toList();
-//     final textsData = texts.map((e) => e.toJson()).toList();
-//     final drawingData = {
-//       'points': pointsData,
-//       'texts': textsData,
-//       'color': color.value,
-//       'backgroundColor': backgroundColor.value,
-//
-//     };
-//     prefs.setString(filename, json.encode(drawingData));
-//   }
-//
-//   Future<Map<String, dynamic>> loadDrawing(String filename) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     final drawingData = prefs.getString(filename);
-//     if (drawingData != null) {
-//       return json.decode(drawingData);
-//     }
-//     return {};
-//   }
-//
-//   Future<List<String>> listDrawings() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     return prefs.getKeys().toList();
-//   }
-// }
 import 'dart:convert';
 import 'dart:ui';
 import 'package:shared_preferences/shared_preferences.dart';
